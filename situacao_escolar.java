1340a5e278b9d8f0879624a3cc1c966ac364ff05
Gustavo Rodrigues da Silva <gustavo.aprendiz09@gmail.com>
2026-03-30 16:10:06 -0300

    Refactor input prompts and concept assignment

--- situacao_escolar.java
+++ situacao_escolar.java
@@ -1,34 +1,35 @@
-import static java.lang.IO.*;
+import static java.lang.IO.readln;
 void main() {
-    IO.println("digite a nota 1");
-      float not1 = Float.parseFloat(readln());
-    IO.println("digite a nota 2");
-      float not2 = Float.parseFloat(readln());
-    IO.println("digite a nota 3");
-      float not3 = Float.parseFloat(readln());
-    IO.println("digite a nota 4");
-      float not4 = Float.parseFloat(readln());
-        float media = not1 + not2 + not3 + not4
+    IO.println("digite a nota 1: ");
+        float not1 = Float.parseFloat(readln());
+    IO.println("digite a nota 2: ");
+        float not2 = Float.parseFloat(readln());
+    IO.println("digite a nota 3: ");
+        float not3 = Float.parseFloat(readln());
+    IO.println("digite a nota 4: ");
+        float not4 = Float.parseFloat(readln());
+        float media = not1 + not2 + not3 + not4;
+        char conceito;
     
     if (media >= 9.0 && media <= 10.0) {
-    conceito = 'A';
+        conceito = 'A';
     } else if (media >= 7.5) {
-    conceito = 'B';
+        conceito = 'B';
     } else if (media >= 6.0) {
-    conceito = 'C';
+        conceito = 'C';
     } else if (media >= 4.0) {
-    conceito = 'D';
+        conceito = 'D';
     } else {
-    conceito = 'E';
+        conceito = 'E';
     }
 
     String situacao;
 
     if (conceito == 'A' || conceito == 'B' || conceito == 'C') {
-    situacao = "APROVADO";
+        situacao = "APROVADO";
     } else {
-    situacao = "REPROVADO";
-    
+        situacao = "REPROVADO";
+
 
     }
 }
