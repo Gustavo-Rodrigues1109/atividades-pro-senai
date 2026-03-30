271729e7f1a8cd50f84ca014b715fb51b6e6ce30
Gustavo Rodrigues da Silva <gustavo.aprendiz09@gmail.com>
2026-03-30 14:41:12 -0300

    1

---
+++ TurnoEstudo.java
@@ -0,0 +1,20 @@
+import static java.lang.IO.*;
+void main() {
+
+    println("Digite o turno do dia em que voce estuda (M , T ou N ");
+      String turno_de_estudo = readln();
+
+    if (turno_de_estudo.equals("M")){
+        println("Bom dia");
+    }
+
+    else if (turno_de_estudo.equals("T")) {
+     println("Boa tarde");
+    }
+else if (turno_de_estudo.equals("N")) {
+    println(" Boa noite");
+    }
+else{
+    println("Valor invalido");
+    }
+}
