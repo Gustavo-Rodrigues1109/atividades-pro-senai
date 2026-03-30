194235349c3c9356a543875946fd47cc3cad20e5
Gustavo Rodrigues da Silva <gustavo.aprendiz09@gmail.com>
2026-03-30 16:06:46 -0300

    Update situacao_escolar.java

--- situacao_escolar.java
+++ situacao_escolar.java
@@ -1,55 +1,34 @@
 import static java.lang.IO.*;
 void main() {
     IO.println("digite a nota 1");
-      float nota1 = Float.parseFloat(readln());
+      float not1 = Float.parseFloat(readln());
     IO.println("digite a nota 2");
-      float nota2 = Float.parseFloat(readln());
+      float not2 = Float.parseFloat(readln());
     IO.println("digite a nota 3");
-      float nota3 = Float.parseFloat(readln());
+      float not3 = Float.parseFloat(readln());
     IO.println("digite a nota 4");
-      float nota4 = Float.parseFloat(readln());
-      float media_final = (nota1 + nota2 + nota3 + nota4) / 4;
-  
-    if (media_final <= 3.9) {
-          IO.println(nota1);
-          IO.println(nota2);
-          IO.println(nota3);
-          IO.println(nota4);
-          IO.println("Sua media final foi :     " + media_final);
-          IO.println("Conceito : E");
-          IO.println("VocÊ esta reprovado");
-    } else if (media_final <= 5.9) {
-          IO.println(nota1);
-          IO.println(nota2);
-          IO.println(nota3);
-          IO.println(nota4);
-          IO.println("Sua media final foi :     " + media_final);
-          IO.println("Conceito : D");
-          IO.println("Você esta reprovado");
-    } else if (media_final <= 7.4) {
-          IO.println(nota1);
-          IO.println(nota2);
-          IO.println(nota3);
-          IO.println(nota4);
-          IO.println("Sua media final foi :     " + media_final);
-          IO.println("Conceito : C");
-          IO.println("Você esta aprovado");
-    } else if (media_final <= 8.9) {
-          IO.println(nota1);
-          IO.println(nota2);
-          IO.println(nota3);
-          IO.println(nota4);
-          IO.println("Sua media final foi :     " + media_final);
-          IO.println("Conceito : B");
-          IO.println("Você esta aprovado");
+      float not4 = Float.parseFloat(readln());
+        float media = not1 + not2 + not3 + not4
+    
+    if (media >= 9.0 && media <= 10.0) {
+    conceito = 'A';
+    } else if (media >= 7.5) {
+    conceito = 'B';
+    } else if (media >= 6.0) {
+    conceito = 'C';
+    } else if (media >= 4.0) {
+    conceito = 'D';
+    } else {
+    conceito = 'E';
     }
-else  {
-        IO.println(nota1);
-        IO.println(nota2);
-        IO.println(nota3);
-        IO.println(nota4);
-        IO.println("Sua media final foi :     " + media_final);
-        IO.println("Conceito : A");
-        IO.println("Você esta aprovado");
+
+    String situacao;
+
+    if (conceito == 'A' || conceito == 'B' || conceito == 'C') {
+    situacao = "APROVADO";
+    } else {
+    situacao = "REPROVADO";
+    
+
     }
 }
