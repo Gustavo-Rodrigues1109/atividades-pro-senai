d29eef56f06bedd8530ef44fa788b7ac412daaf3
Gustavo Rodrigues da Silva <gustavo.aprendiz09@gmail.com>
2026-03-30 16:53:33 -0300

    Refactor situacao_escolar to use Scanner for input
    Refactor the program to use Scanner for input and calculate average correctly.

--- situacao_escolar.java
+++ situacao_escolar.java
@@ -1,35 +1,50 @@
-import static java.lang.IO.readln;
-void main() {
-    IO.println("digite a nota 1: ");
-        float not1 = Float.parseFloat(readln());
-    IO.println("digite a nota 2: ");
-        float not2 = Float.parseFloat(readln());
-    IO.println("digite a nota 3: ");
-        float not3 = Float.parseFloat(readln());
-    IO.println("digite a nota 4: ");
-        float not4 = Float.parseFloat(readln());
-        float media = not1 + not2 + not3 + not4;
-        char conceito;
-    
-    if (media >= 9.0 && media <= 10.0) {
-        conceito = 'A';
-    } else if (media >= 7.5) {
-        conceito = 'B';
-    } else if (media >= 6.0) {
-        conceito = 'C';
-    } else if (media >= 4.0) {
-        conceito = 'D';
-    } else {
-        conceito = 'E';
-    }
+import java.util.Scanner;
 
-    String situacao;
+public class Prestacao {
+    public static void main(String[] args) {
 
-    if (conceito == 'A' || conceito == 'B' || conceito == 'C') {
-        situacao = "APROVADO";
-    } else {
-        situacao = "REPROVADO";
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Digite a nota 1: ");
+            float not1 = sc.nextFloat();
+
+        System.out.print("Digite a nota 2: ");
+            float not2 = sc.nextFloat();
+
+        System.out.print("Digite a nota 3: ");
+            float not3 = sc.nextFloat();
+
+        System.out.print("Digite a nota 4: ");
+            float not4 = sc.nextFloat();
+
+            float media = (not1 + not2 + not3 + not4) / 4;
+
+            char conceito;
+
+        if (media >= 9.0 && media <= 10.0) {
+            conceito = 'A';
+        } else if (media >= 7.5) {
+            conceito = 'B';
+        } else if (media >= 6.0) {
+            conceito = 'C';
+        } else if (media >= 4.0) {
+            conceito = 'D';
+        } else {
+            conceito = 'E';
+        }
+
+        String situacao;
+
+        if (conceito == 'A' || conceito == 'B' || conceito == 'C') {
+            situacao = "APROVADO";
+        } else {
+            situacao = "REPROVADO";
+        }
+        
+        System.out.println("\nNotas: " + not1 + ", " + not2 + ", " + not3 + ", " + not4);
+        System.out.println("Média: " + media);
+        System.out.println("Conceito: " + conceito);
+        System.out.println("Situação: " + situacao);
 
     }
 }
