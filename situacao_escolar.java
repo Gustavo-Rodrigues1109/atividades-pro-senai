3d009b5ba5bcdb1de62bbd7e832adb677aed0442
Gustavo Rodrigues da Silva <gustavo.aprendiz09@gmail.com>
2026-03-30 15:34:09 -0300

    Update situacao_escolar.java

--- situacao_escolar.java
+++ situacao_escolar.java
@@ -1,55 +1,55 @@
 import static java.lang.IO.*;
 void main() {
-    println("digite a nota 1");
+    IO.println("digite a nota 1");
       float nota1 = Float.parseFloat(readln());
-    println("digite a nota 2");
+    IO.println("digite a nota 2");
       float nota2 = Float.parseFloat(readln());
-    println("digite a nota 3");
+    IO.println("digite a nota 3");
       float nota3 = Float.parseFloat(readln());
-    println("digite a nota 4");
+    IO.println("digite a nota 4");
       float nota4 = Float.parseFloat(readln());
       float media_final = (nota1 + nota2 + nota3 + nota4) / 4;
   
     if (media_final <= 3.9) {
-          println(nota1);
-          println(nota2);
-          println(nota3);
-          println(nota4);
-          println("Sua media final foi :     " + media_final);
-          println("Categoria : E");
-          println("VocÊ esta reprovado");
+          IO.println(nota1);
+          IO.println(nota2);
+          IO.println(nota3);
+          IO.println(nota4);
+          IO.println("Sua media final foi :     " + media_final);
+          IO.println("Conceito : E");
+          IO.println("VocÊ esta reprovado");
     } else if (media_final <= 5.9) {
-          println(nota1);
-          println(nota2);
-          println(nota3);
-          println(nota4);
-          println("Sua media final foi :     " + media_final);
-          println("Categoria : D");
-          println("Você esta reprovado");
+          IO.println(nota1);
+          IO.println(nota2);
+          IO.println(nota3);
+          IO.println(nota4);
+          IO.println("Sua media final foi :     " + media_final);
+          IO.println("Conceito : D");
+          IO.println("Você esta reprovado");
     } else if (media_final <= 7.4) {
-          println(nota1);
-          println(nota2);
-          println(nota3);
-          println(nota4);
-          println("Sua media final foi :     " + media_final);
-          println("Categoria : C");
-          println("Você esta aprovado");
+          IO.println(nota1);
+          IO.println(nota2);
+          IO.println(nota3);
+          IO.println(nota4);
+          IO.println("Sua media final foi :     " + media_final);
+          IO.println("Conceito : C");
+          IO.println("Você esta aprovado");
     } else if (media_final <= 8.9) {
-          println(nota1);
-          println(nota2);
-          println(nota3);
-          println(nota4);
-          println("Sua media final foi :     " + media_final);
-          println("Categoria : B");
-          println("Você esta aprovado");
+          IO.println(nota1);
+          IO.println(nota2);
+          IO.println(nota3);
+          IO.println(nota4);
+          IO.println("Sua media final foi :     " + media_final);
+          IO.println("Conceito : B");
+          IO.println("Você esta aprovado");
     }
 else  {
-        println(nota1);
-        println(nota2);
-        println(nota3);
-        println(nota4);
-        println("Sua media final foi :     " + media_final);
-        println("Categoria : A");
-        println("Você esta aprovado");
+        IO.println(nota1);
+        IO.println(nota2);
+        IO.println(nota3);
+        IO.println(nota4);
+        IO.println("Sua media final foi :     " + media_final);
+        IO.println("Conceito : A");
+        IO.println("Você esta aprovado");
     }
 }
