aa0f17a57e0adef792ca0d5fcc0706e48fcd96bd
Gustavo Rodrigues da Silva <gustavo.aprendiz09@gmail.com>
2026-03-30 14:45:40 -0300

    1

--- CalculadoraSimples.java
+++ CalculadoraSimples.java
@@ -17,35 +17,35 @@ void main() {
 
     println("insira um valor das operacoes acima");
       String operaracao = readln();
-    if (operaracao.equals("1")){
+    if (operacao.equals("1")){
           float soma = valor1 + valor2;
         println(soma);
     }
 
-    else if (operaracao.equals("2")) {
+    else if (operacao.equals("2")) {
 
           float subtracao = valor1 - valor2;
         println(subtracao);
 
     }
 
-    else if (operaracao.equals("3")) {
+    else if (operacao.equals("3")) {
 
           float multiplicacao = valor1 * valor2;
         println(multiplicacao);
     }
-    else if (operaracao.equals("4")) {
+    else if (operacao.equals("4")) {
 
           float divisao = valor1 / valor2;
         println(divisao);
     }
-    else if (operaracao.equals("5")) {
+    else if (operacao.equals("5")) {
 
           double potencia = Math.pow(valor1 , valor2 );
         println(potencia);
     }
 
-    else if (operaracao.equals("6")) {
+    else if (operacao.equals("6")) {
           float soma2 = valor1 + valor2;
           double raiz_quadrada = Math.sqrt(soma2);
         println(raiz_quadrada);
